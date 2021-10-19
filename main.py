4ac265ca49a17842051fb08ec6c137b6c941ebae
David Dinh <dndinh7@gmail.com>
2021-10-19 17:07:16 -0400

    adding inputs from the terminal command

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import re
 import os
+import sys
 
-def main(constraints, preferences, schedule):
+def main():
+    constraints_input= sys.argv[1]
 
+    with open(sys.argv[1], 'r') as file:
+    preferences_input= sys.argv[2]
+
+
+    schedule_output= sys.argv[3]
 
 if __name__ == "__main__":
+
+    main()
